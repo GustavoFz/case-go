7a3f931ba1d9e7cf5e773de24b6643ef6b4b2f00
Gustavo Franco <gustavofrancojp@gmail.com>
2022-11-04 01:01:38 -0400

    Add status in ProductResponse

--- responses/product.go
+++ responses/product.go
@@ -3,6 +3,7 @@ package responses
 import "github.com/labstack/echo/v4"
 
 type ProductResponse struct {
-	Message string    `json:"message"`
-	Data    *echo.Map `json:"data"`
+	Status   int       `json:"status"`
+	Message  string    `json:"message"`
+	Response *echo.Map `json:"response"`
 }
